bf5ec265a7c64b018503d0961f810f988b9de506
Clemence Yun Chan Lee <yunchan.c.lee@gmail.com>
2017-09-26 20:55:15 -0500

    feat(ngrx): add ngrx router and dev tool modules
    https://app.asana.com/0/432385136126911/438502122600359

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,9 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import {ClarityModule} from 'clarity-angular';
+import {StoreModule} from '@ngrx/store';
+import {routerReducer} from '@ngrx/router-store';
+import {StoreDevtoolsModule} from '@ngrx/store-devtools';
 
 @NgModule({
   declarations: [
@@ -10,6 +13,10 @@ import {ClarityModule} from 'clarity-angular';
   ],
   imports: [
     BrowserModule,
+    StoreModule.forRoot({ routerReducer: routerReducer }),
+    StoreDevtoolsModule.instrument({
+      maxAge: 25
+    }),
     ClarityModule.forRoot(),
   ],
   providers: [],
