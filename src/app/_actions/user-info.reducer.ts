8a41ea9c40b6557952000c134af310f413ec967f
Clemence Yun Chan Lee <yunchan.c.lee@gmail.com>
2017-10-09 21:08:20 -0500

    feat(user-info): add reducer & actions

--- src/app/_actions/user-info.reducer.ts
+++ src/app/_actions/user-info.reducer.ts
@@ -1,3 +1,17 @@
-/**
- * Created by yclee on 09/10/2017.
- */
+import {Action, ActionReducer} from '@ngrx/store';
+import * as UserInfoActions from './user-info.actions';
+import * as Crud from './crud.reducer';
+import {UserInfo} from '../_domains/user-info';
+
+const prefix = UserInfoActions.ACTION_NAME;
+const INIT = 'INIT';
+const initialState = {
+  phoneNumber: null,
+  notification: null
+};
+
+export const reducer: ActionReducer<UserInfo> = (state = initialState, action: Action = {type: INIT}) => {
+  const newState = Crud.reducer(state, action, prefix, initialState);
+
+  return newState === null ? initialState : newState;
+};
