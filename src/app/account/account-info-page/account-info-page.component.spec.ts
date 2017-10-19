828f417115655693122be2e7ab1ce0697d373f46
Clemence Yun Chan Lee <yunchan.c.lee@gmail.com>
2017-10-19 18:23:23 -0500

    fix(account-info-page): fix broken spec

--- src/app/account/account-info-page/account-info-page.component.spec.ts
+++ src/app/account/account-info-page/account-info-page.component.spec.ts
@@ -1,20 +1,20 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import {async, ComponentFixture, TestBed} from '@angular/core/testing';
 
-import { ProfileComponent } from './account-info-page.component';
+import {AccountInfoPageComponent} from './account-info-page.component';
 
-describe('ProfileComponent', () => {
-  let component: ProfileComponent;
-  let fixture: ComponentFixture<ProfileComponent>;
+describe('AccountInfoPageComponent', () => {
+  let component: AccountInfoPageComponent;
+  let fixture: ComponentFixture<AccountInfoPageComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ ProfileComponent ]
+      declarations: [AccountInfoPageComponent]
     })
-    .compileComponents();
+      .compileComponents();
   }));
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(ProfileComponent);
+    fixture = TestBed.createComponent(AccountInfoPageComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
