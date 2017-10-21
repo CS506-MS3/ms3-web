bbae39366621a7c50e8dd6cf340750976c2ed130
Clemence Yun Chan Lee <yunchan.c.lee@gmail.com>
2017-10-21 13:44:14 -0500

    feat(access-purchase-form): integrate stripe checkout

--- src/app/access-purchase/access-purchase-form/access-purchase-form.component.ts
+++ src/app/access-purchase/access-purchase-form/access-purchase-form.component.ts
@@ -1,4 +1,4 @@
-import {ChangeDetectionStrategy, Component, Input, OnInit} from '@angular/core';
+import {ChangeDetectionStrategy, Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
 
 @Component({
   selector: 'app-access-purchase-form',
@@ -8,7 +8,10 @@ import {ChangeDetectionStrategy, Component, Input, OnInit} from '@angular/core';
 })
 export class AccessPurchaseFormComponent implements OnInit {
   @Input() item: any;
+  @Output() onTokenReceived: EventEmitter<any> = new EventEmitter<any>();
+
   count = 1;
+  hasToken = false;
 
   constructor() { }
 
@@ -17,9 +20,42 @@ export class AccessPurchaseFormComponent implements OnInit {
 
   incrementCount() {
     this.count++;
+    this.hasToken = false;
+    this.onTokenReceived.emit({
+      item: this.item,
+      count: this.count,
+      token: null
+    });
   }
 
   decrementCount() {
     this.count -= 1 < this.count ? 1 : 0;
+    this.hasToken = false;
+    this.onTokenReceived.emit({
+      item: this.item,
+      count: this.count,
+      token: null
+    });
+  }
+
+  openCheckout() { // based on http://blog.mgechev.com/2016/07/05/using-stripe-payment-with-angular-2/
+    const handler = (<any>window).StripeCheckout.configure({
+      key: 'pk_test_oi0sKPJYLGjdvOXOM8tE8cMa',
+      locale: 'auto',
+      token: function (token: any) {
+        this.onTokenReceived.emit({
+          item: this.item,
+          count: this.count,
+          token: token
+        });
+        this.hasToken = true;
+      }
+    });
+
+    handler.open({
+      name: 'Madison Sublease',
+      description: this.item.title,
+      amount: this.count * this.item.pricePerItem * 100
+    });
   }
 }
