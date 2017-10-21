caae464e0f9174de97d0a8cec07e6dfe9994800f
Clemence Yun Chan Lee <yunchan.c.lee@gmail.com>
2017-10-21 16:11:11 -0500

    feat(access-add.effects): add api effects

--- src/app/access-purchase/access-purchase.module.ts
+++ src/app/access-purchase/access-purchase.module.ts
@@ -5,13 +5,15 @@ import {ClarityModule} from 'clarity-angular';
 import { AccessPurchaseFormComponent } from './access-purchase-form/access-purchase-form.component';
 import {EffectsModule} from '@ngrx/effects';
 import {AccessItemEffects} from './access-item.effects';
+import {AccessAddEffects} from './access-add.effects';
 
 @NgModule({
   imports: [
     CommonModule,
     ClarityModule.forChild(),
     EffectsModule.forFeature([
-      AccessItemEffects
+      AccessItemEffects,
+      AccessAddEffects
     ])
   ],
   declarations: [AccessPurchasePageComponent, AccessPurchaseFormComponent]
