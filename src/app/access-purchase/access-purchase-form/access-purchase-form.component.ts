8079a3b97cf6be68c433a184201a7ee7ea6ca75c
Clemence Yun Chan Lee <yunchan.c.lee@gmail.com>
2017-10-21 13:27:10 -0500

    feat(access-purchase-form): add input binding and counter operations

--- src/app/access-purchase/access-purchase-form/access-purchase-form.component.ts
+++ src/app/access-purchase/access-purchase-form/access-purchase-form.component.ts
@@ -1,15 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import {ChangeDetectionStrategy, Component, Input, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-access-purchase-form',
+  changeDetection: ChangeDetectionStrategy.OnPush,
   templateUrl: './access-purchase-form.component.html',
   styleUrls: ['./access-purchase-form.component.scss']
 })
 export class AccessPurchaseFormComponent implements OnInit {
+  @Input() item: any;
+  count = 1;
 
   constructor() { }
 
   ngOnInit() {
   }
 
+  incrementCount() {
+    this.count++;
+  }
+
+  decrementCount() {
+    this.count -= 1 < this.count ? 1 : 0;
+  }
 }
