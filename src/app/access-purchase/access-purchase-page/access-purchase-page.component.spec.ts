50ed96da264579e3546e7f8411bbba304947bec6
Clemence Yun Chan Lee <yunchan.c.lee@gmail.com>
2017-10-21 14:02:47 -0500

    feat(access-purchase-page): add token received handling and onSubmit method

--- src/app/access-purchase/access-purchase-page/access-purchase-page.component.spec.ts
+++ src/app/access-purchase/access-purchase-page/access-purchase-page.component.spec.ts
@@ -2,15 +2,22 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { AccessPurchasePageComponent } from './access-purchase-page.component';
 import {CUSTOM_ELEMENTS_SCHEMA} from '@angular/core';
+import {Store} from '@ngrx/store';
 
 describe('AccessPurchasePageComponent', () => {
   let component: AccessPurchasePageComponent;
   let fixture: ComponentFixture<AccessPurchasePageComponent>;
+  const mockStore = {
+    dispatch: jasmine.createSpy('dispatch')
+  };
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       schemas: [
         CUSTOM_ELEMENTS_SCHEMA
+      ],
+      providers: [
+        {provide: Store, useValue: mockStore}
       ],
       declarations: [ AccessPurchasePageComponent ]
     })
@@ -29,7 +36,81 @@ describe('AccessPurchasePageComponent', () => {
   });
 
   describe('onStripeTokenReceived', () => {
-    it('should do something', () => {
+    it('should set purchase form to the data input if data has token', () => {
+      const testPurchaseData = {
+        item: {
+          id: 1,
+          title: 'Subleaser Access Subscription',
+          pricePerItem: 10.99,
+          canHaveMultiple: false
+        },
+        count: 1,
+        token: {
+          id: 'someToken',
+          email: 'test@wisc.edu'
+        }
+      };
+
+      component.onStripeTokenReceived(testPurchaseData);
+
+      expect(component.hasToken).toBeTruthy();
+      expect(component.purchaseForm).toEqual({
+        id: testPurchaseData.item.id,
+        count: testPurchaseData.count,
+        token: testPurchaseData.token
+      });
+    });
+
+    it('should reset token status & form if data input has no token', () => {
+      const testPurchaseData = {
+        item: {
+          id: 1,
+          title: 'Subleaser Access Subscription',
+          pricePerItem: 10.99,
+          canHaveMultiple: false
+        },
+        count: 1,
+        token: {
+          id: 'someToken',
+          email: 'test@wisc.edu'
+        }
+      };
+      component.onStripeTokenReceived(testPurchaseData);
+
+      const updatedTestPurchaseData = {
+        item: {
+          id: 1,
+          title: 'Subleaser Access Subscription',
+          pricePerItem: 10.99,
+          canHaveMultiple: false
+        },
+        count: 2,
+        token: null
+      };
+      component.onStripeTokenReceived(updatedTestPurchaseData);
+
+      expect(component.hasToken).toBeFalsy();
+      expect(component.purchaseForm).toBeNull();
+    });
+  });
+
+  describe('onSubmit', () => {
+    it('should dispatch a AccessPurchaseEffects.Request', () => {
+      component.purchaseForm = {
+        id: 1,
+        count: 1,
+        token: {
+          id: 'someToken',
+          email: 'test@wisc.edu'
+        }
+      };
+
+      component.onSubmit();
+
+      expect(mockStore.dispatch).toHaveBeenCalledWith({
+        type: 'AccessPurchaseEffects.REQUEST',
+        payload: component.purchaseForm
+      })
     });
   });
 });
