61321ec5b7a4bb020a86c17ee28bd0550278441b
Clemence Yun Chan Lee <yunchan.c.lee@gmail.com>
2017-10-25 13:58:02 -0500

    fix(access-add): add error handling

--- src/app/access-purchase/access-purchase.module.ts
+++ src/app/access-purchase/access-purchase.module.ts
@@ -2,9 +2,9 @@ import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {AccessPurchasePageComponent} from './access-purchase-page/access-purchase-page.component';
 import {ClarityModule} from 'clarity-angular';
-import { AccessPurchaseFormComponent } from './access-purchase-form/access-purchase-form.component';
+import {AccessPurchaseFormComponent} from './access-purchase-form/access-purchase-form.component';
 import {EffectsModule} from '@ngrx/effects';
-import {AccessItemEffects} from './pricings-get.effects';
+import {PricingsGetEffects} from './pricings-get.effects';
 import {AccessAddEffects} from './access-add.effects';
 
 @NgModule({
@@ -12,7 +12,7 @@ import {AccessAddEffects} from './access-add.effects';
     CommonModule,
     ClarityModule.forChild(),
     EffectsModule.forFeature([
-      AccessItemEffects,
+      PricingsGetEffects,
       AccessAddEffects
     ])
   ],
