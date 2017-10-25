0587e8e46c3e819bd1238ba99cff70ac74b82724
Clemence Yun Chan Lee <yunchan.c.lee@gmail.com>
2017-10-25 13:24:25 -0500

    feat(domain): add Pricing domain

---
+++ src/app/_domains/pricing.ts
@@ -0,0 +1,7 @@
+export interface Pricing {
+  id: number;
+  type: 'VENDOR_SUBSCRIPTION' | 'VENDOR_ADDITIONAL' | 'CUSTOMER_SUBSCRIPTION'
+  alias: string;
+  pricePerItem: number;
+  canHaveMultiple: boolean;
+}
