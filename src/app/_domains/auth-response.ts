d5754ea236c614e0d0686fe2736ede9154689f34
Clemence Yun Chan Lee <yunchan.c.lee@gmail.com>
2017-10-25 11:36:18 -0500

    feat(domain): defined auth api response data structure

---
+++ src/app/_domains/auth-response.ts
@@ -0,0 +1,10 @@
+import {PropertySummary} from './property-summary';
+
+export interface AuthResponse {
+  token: string;
+  user: {
+    id: number;
+    email: string;
+    wishlist: PropertySummary[];
+  }
+}
