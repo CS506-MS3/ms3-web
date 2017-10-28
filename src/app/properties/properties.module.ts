e80e3cb10d09ac1584b5811ba0fe8c060411158c
Naman Maheshwari <namanmaheshwari97@gmail.com>
2017-10-28 16:28:09 -0500

    Fixed clr-icon issue and added dropdown

--- src/app/properties/properties.module.ts
+++ src/app/properties/properties.module.ts
@@ -3,10 +3,12 @@ import { CommonModule } from '@angular/common';
 import { PropertiesService } from './properties.service';
 import { PropertyService } from './property.service';
 import { CreateListingPageComponent} from './create-listing-page/create-listing-page.component';
+import {ClarityModule} from "clarity-angular";
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    ClarityModule.forChild()
   ],
   declarations: [CreateListingPageComponent],
   providers: [PropertiesService, PropertyService]
