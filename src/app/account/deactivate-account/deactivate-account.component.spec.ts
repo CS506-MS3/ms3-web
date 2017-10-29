11bacd0bfafcfad20d8480cdef8e159c8199cc96
Clemence Yun Chan Lee <yunchan.c.lee@gmail.com>
2017-10-29 10:53:45 -0500

    fix(deactivate): fix spec error

--- src/app/account/deactivate-account/deactivate-account.component.spec.ts
+++ src/app/account/deactivate-account/deactivate-account.component.spec.ts
@@ -2,6 +2,7 @@ import {async, ComponentFixture, TestBed} from '@angular/core/testing';
 
 import {DeactivateAccountComponent} from './deactivate-account.component';
 import {AccountService} from '../account.service';
+import {ReactiveFormsModule} from '@angular/forms';
 
 describe('DeactivateAccountComponent', () => {
   const mockAccountService = {
@@ -12,6 +13,9 @@ describe('DeactivateAccountComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
+      imports: [
+        ReactiveFormsModule
+      ],
       providers: [
         {provide: AccountService, useValue: mockAccountService}
       ],
