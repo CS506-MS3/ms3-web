a7a10c2e8d3b8c7a6e58a1d64e0cb417fbac694b
Clemence Yun Chan Lee <yunchan.c.lee@gmail.com>
2017-10-30 12:47:59 -0500

    feat(address-verification-form): add address verification form

---
+++ src/app/properties/address-verification-form/address-verification-form.component.spec.ts
@@ -0,0 +1,25 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { AddressVerificationFormComponent } from './address-verification-form.component';
+
+describe('AddressVerificationFormComponent', () => {
+  let component: AddressVerificationFormComponent;
+  let fixture: ComponentFixture<AddressVerificationFormComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ AddressVerificationFormComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(AddressVerificationFormComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
