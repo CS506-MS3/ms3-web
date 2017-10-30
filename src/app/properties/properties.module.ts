ced2e5c3bdfbaa61051b4a8c1eab00b36e44082e
Clemence Yun Chan Lee <yunchan.c.lee@gmail.com>
2017-10-30 12:49:17 -0500

    feat(properties.module): register components

--- src/app/properties/properties.module.ts
+++ src/app/properties/properties.module.ts
@@ -1,16 +1,27 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { PropertiesService } from './properties.service';
-import { PropertyService } from './property.service';
-import { CreateListingPageComponent} from './create-listing-page/create-listing-page.component';
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {PropertiesService} from './properties.service';
+import {PropertyService} from './property.service';
+import {CreateListingPageComponent} from './create-listing-page/create-listing-page.component';
 import {ClarityModule} from "clarity-angular";
+import {PropertyFormComponent} from './property-form/property-form.component';
+import {AddressVerificationFormComponent} from './address-verification-form/address-verification-form.component';
+import {ImageFormComponent} from './image-form/image-form.component';
+import {ReactiveFormsModule} from '@angular/forms';
 
 @NgModule({
   imports: [
     CommonModule,
+    ReactiveFormsModule,
     ClarityModule.forChild()
   ],
-  declarations: [CreateListingPageComponent],
+  declarations: [
+    CreateListingPageComponent,
+    PropertyFormComponent,
+    AddressVerificationFormComponent,
+    ImageFormComponent
+  ],
   providers: [PropertiesService, PropertyService]
 })
-export class PropertiesModule { }
+export class PropertiesModule {
+}
