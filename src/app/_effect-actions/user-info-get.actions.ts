82441143afdfe604b27216e01b7475d331726904
Clemence Yun Chan Lee <yunchan.c.lee@gmail.com>
2017-10-31 12:51:08 -0500

    feat(user-info-get): add api actions for user info get

---
+++ src/app/_effect-actions/user-info-get.actions.ts
@@ -0,0 +1,27 @@
+import {Action} from '@ngrx/store';
+import {RequestError} from '../_domains/request-error';
+
+export const REQUEST = 'UserInfoGet.REQUEST';
+export const SUCCESS = 'UserInfoGet.SUCCESS';
+export const ERROR = 'UserInfoGet.ERROR';
+
+export class Request implements Action {
+  readonly type = REQUEST;
+
+  constructor(public payload: number) {
+  }
+}
+
+export class Success implements Action {
+  readonly type = SUCCESS;
+
+  constructor(public payload) {
+  }
+}
+
+export class Error implements Action {
+  readonly type = ERROR;
+
+  constructor(public payload: RequestError) {
+  }
+}
