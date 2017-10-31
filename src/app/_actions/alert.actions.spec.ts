bde2a7af1a2ab745630aa35203947c0a39b3705c
Clemence Yun Chan Lee <yunchan.c.lee@gmail.com>
2017-10-30 22:37:35 -0500

    test(alert.actions): remove obsolete test

--- src/app/_actions/alert.actions.spec.ts
+++ src/app/_actions/alert.actions.spec.ts
@@ -5,15 +5,6 @@ describe('AlertActions', () => {
   const testAppAlert = new AppAlert(true, 'ERROR', 'Some Error Message');
 
   describe('Actions', () => {
-    describe('SetAlert', () => {
-      it('should have type SET_ALERT, and require AppAlert payload', () => {
-        const action = new AlertActions.SetAlert(testAppAlert);
-
-        expect(action.type).toEqual(AlertActions.SET_ALERT);
-        expect(action.payload).toEqual(testAppAlert);
-      });
-    });
-
     describe('HideAlert', () => {
       it('should have type HIDE_ALERT, and not require AppAlert payload', () => {
         const action = new AlertActions.HideAlert();
@@ -24,16 +15,6 @@ describe('AlertActions', () => {
   });
 
   describe('Reducer', () => {
-    it('should return a new SetAlert with the input payload when action type = SetAlert', () => {
-      const initialState = new AppAlert();
-      const inputAction = new AlertActions.SetAlert(testAppAlert);
-
-      const outputState = AlertActions.reducer(initialState, inputAction);
-
-      expect(outputState.constructor.name).toEqual('AppAlert');
-      expect(outputState).toEqual(testAppAlert);
-    });
-
     it('should return the default empty alert if HideAlert is given', () => {
       const initialState = testAppAlert;
       const inputAction = new AlertActions.HideAlert();
