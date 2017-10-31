9bc6ebb1d5853b5b8d8ca976fa08c6fb6114aa3a
Clemence Yun Chan Lee <yunchan.c.lee@gmail.com>
2017-10-30 23:41:00 -0500

    feat(sign-up): add progress component to each stage

--- src/app/account/activate-page/activate-page.component.ts
+++ src/app/account/activate-page/activate-page.component.ts
@@ -1,12 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, HostBinding, OnInit} from '@angular/core';
 import {AccountService} from '../account.service';
-// Empty page object (or could have a loading bar?) to provide inlet for activation link
+
 @Component({
   selector: 'app-activate-page',
   templateUrl: './activate-page.component.html',
   styleUrls: ['./activate-page.component.scss']
 })
 export class ActivatePageComponent implements OnInit {
+  @HostBinding('class') cssClass = 'content-container';
 
   constructor(private _account: AccountService) { }
 
