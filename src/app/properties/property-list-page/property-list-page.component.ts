c39e85f250c914c2b231e56f32167e04c5026a0c
Clemence Yun Chan Lee <yunchan.c.lee@gmail.com>
2017-11-10 10:55:27 -0600

    refact(property-list-page): add dummy data

--- src/app/properties/property-list-page/property-list-page.component.ts
+++ src/app/properties/property-list-page/property-list-page.component.ts
@@ -1,4 +1,5 @@
 import {Component, HostBinding, OnInit} from '@angular/core';
+import {Properties} from '../../_domains/properties';
 
 @Component({
   selector: 'app-property-list-page',
@@ -8,16 +9,70 @@ import {Component, HostBinding, OnInit} from '@angular/core';
 export class PropertyListPageComponent implements OnInit {
   @HostBinding('class') cssClass = 'content-container';
 
-  data: number[] = [];
+  data: Properties = {
+    list: [
+      {
+        id: 1,
+        title: 'Lorem Ipsum Dolores',
+        address: '123 Test Ave., Madison, WI',
+        price: 500,
+        startDate: '2017-10-01T00:00:00',
+        duration: 6,
+        thumbnailUrl: 'assets/images/eachProperty.jpg'
+      },
+      {
+        id: 2,
+        title: 'Lorem Ipsum Dolores',
+        address: '123 Test Ave., Madison, WI',
+        price: 500,
+        startDate: '2017-10-01T00:00:00',
+        duration: 6,
+        thumbnailUrl: 'assets/images/eachProperty.jpg'
+      },
+      {
+        id: 3,
+        title: 'Lorem Ipsum Dolores',
+        address: '123 Test Ave., Madison, WI',
+        price: 500,
+        startDate: '2017-10-01T00:00:00',
+        duration: 6,
+        thumbnailUrl: 'assets/images/eachProperty.jpg'
+      },
+      {
+        id: 4,
+        title: 'Lorem Ipsum Dolores',
+        address: '123 Test Ave., Madison, WI',
+        price: 500,
+        startDate: '2017-10-01T00:00:00',
+        duration: 6,
+        thumbnailUrl: 'assets/images/eachProperty.jpg'
+      },
+      {
+        id: 5,
+        title: 'Lorem Ipsum Dolores',
+        address: '123 Test Ave., Madison, WI',
+        price: 500,
+        startDate: '2017-10-01T00:00:00',
+        duration: 6,
+        thumbnailUrl: 'assets/images/eachProperty.jpg'
+      },
+      {
+        id: 6,
+        title: 'Lorem Ipsum Dolores',
+        address: '123 Test Ave., Madison, WI',
+        price: 500,
+        startDate: '2017-10-01T00:00:00',
+        duration: 6,
+        thumbnailUrl: 'assets/images/eachProperty.jpg'
+      },
+    ]
+  };
   sortOptions = {
     sortBy: 'recent',
     direction: 'UP'
   };
 
   constructor() {
-    for (let i = 0; i < 20; i++) {
-      this.data.push(i);
-    }
   }
 
   ngOnInit() {
