17b5e9add21e380c82b33432c1dcda6ade28265e
Clemence Yun Chan Lee <yunchan.c.lee@gmail.com>
2017-11-10 10:55:04 -0600

    style(property-list-page): formatting

--- src/app/properties/property-list-page/property-list-page.component.spec.ts
+++ src/app/properties/property-list-page/property-list-page.component.spec.ts
@@ -1,6 +1,6 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import {async, ComponentFixture, TestBed} from '@angular/core/testing';
 
-import { PropertyListPageComponent } from './property-list-page.component';
+import {PropertyListPageComponent} from './property-list-page.component';
 import {CUSTOM_ELEMENTS_SCHEMA} from '@angular/core';
 
 describe('PropertyListPageComponent', () => {
@@ -10,9 +10,9 @@ describe('PropertyListPageComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       schemas: [CUSTOM_ELEMENTS_SCHEMA],
-      declarations: [ PropertyListPageComponent ]
+      declarations: [PropertyListPageComponent]
     })
-    .compileComponents();
+      .compileComponents();
   }));
 
   beforeEach(() => {
