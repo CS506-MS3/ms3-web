b9dc5f6739bd7c4cec3521dc2445d7997fc527e7
Clemence Yun Chan Lee <yunchan.c.lee@gmail.com>
2017-11-26 14:43:45 -0600

    feat(password-reset): add UI pages

---
+++ src/app/password-reset/password-reset-page/password-reset-page.component.ts
@@ -0,0 +1,27 @@
+import {Component, OnInit} from '@angular/core';
+import {PasswordResetService} from '../password-reset.service';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+
+@Component({
+  selector: 'app-password-reset-page',
+  templateUrl: './password-reset-page.component.html',
+  styleUrls: ['./password-reset-page.component.scss']
+})
+export class PasswordResetPageComponent implements OnInit {
+  passwordResetForm: FormGroup;
+
+  constructor(private _service: PasswordResetService, private _fb: FormBuilder) {
+    this.passwordResetForm = this._fb.group({
+      password: ['', Validators.required]
+    });
+  }
+
+  ngOnInit() {
+  }
+
+  onSubmit({value, valid}) {
+    if (valid) {
+      this._service.resetPassword(value);
+    }
+  }
+}
