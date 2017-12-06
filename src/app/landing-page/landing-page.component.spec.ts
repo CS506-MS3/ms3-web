867628ac9f497b4a55894e4159cb7b4e12cd73cf
Clemence Yun Chan Lee <yunchan.c.lee@gmail.com>
2017-12-06 12:37:12 -0600

    fix landing page test

--- src/app/landing-page/landing-page.component.spec.ts
+++ src/app/landing-page/landing-page.component.spec.ts
@@ -2,6 +2,7 @@ import {async, ComponentFixture, TestBed} from '@angular/core/testing';
 
 import {LandingPageComponent} from './landing-page.component';
 import {RouterTestingModule} from '@angular/router/testing';
+import {CUSTOM_ELEMENTS_SCHEMA} from '@angular/core';
 
 describe('LandingPageComponent', () => {
   let component: LandingPageComponent;
@@ -9,6 +10,9 @@ describe('LandingPageComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
+      schemas: [
+        CUSTOM_ELEMENTS_SCHEMA
+      ],
       imports: [
         RouterTestingModule
       ],
