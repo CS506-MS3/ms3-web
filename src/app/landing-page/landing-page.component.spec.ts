3915313b988d3720f7ca44a629401c51f735e5ba
Clemence Yun Chan Lee <yunchan.c.lee@gmail.com>
2017-12-11 11:42:39 -0600

    fix spec errors

--- src/app/landing-page/landing-page.component.spec.ts
+++ src/app/landing-page/landing-page.component.spec.ts
@@ -3,10 +3,17 @@ import {async, ComponentFixture, TestBed} from '@angular/core/testing';
 import {LandingPageComponent} from './landing-page.component';
 import {RouterTestingModule} from '@angular/router/testing';
 import {CUSTOM_ELEMENTS_SCHEMA} from '@angular/core';
+import {PropertiesService} from '../properties/properties.service';
+import {Observable} from 'rxjs/Observable';
+import 'rxjs/add/observable/of';
 
 describe('LandingPageComponent', () => {
   let component: LandingPageComponent;
   let fixture: ComponentFixture<LandingPageComponent>;
+  const mockService = {
+    query: jasmine.createSpy('query'),
+    properties$: Observable.of({})
+  };
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
@@ -15,6 +22,9 @@ describe('LandingPageComponent', () => {
       ],
       imports: [
         RouterTestingModule
+      ],
+      providers: [
+        {provide: PropertiesService, useValue: mockService}
       ],
       declarations: [LandingPageComponent]
     })
